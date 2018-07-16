a9781927fb2a3e271099c56a9e3e7c3546ca72a3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-07-16 14:39:24 +0900

    Create test cases for Chap.1 and Chap.2

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,91 @@
+// tslint:disable:no-any
 import * as assert from 'power-assert';
 
 // tslint:disable-next-line:no-relative-imports
-import { sampleFunc } from '../src/index';
+import {
+  IPackage,
+  getPinnedReference,
+  fetchPackage,
+} from '../src/index';
 
-describe('sampleFunc', () => {
-  it('Runable', () => {
-    assert.equal(sampleFunc(1, 2), 3);
+const packageJSON: { dependencies: { [key: string]: string } } = {
+  dependencies: {
+    react: '^15.5.4',
+    'babel-core': '6.25.0',
+  },
+};
+
+describe('mpm', () => {
+  describe('getPinnedReference', () => {
+    it('Semver ^', (done: any) => {
+      getPinnedReference({
+        name: 'react',
+        reference: '^15.5.4',
+      }).then((pkg: IPackage) => {
+        const actual: IPackage = pkg;
+        const expected: IPackage = {
+          name: 'react',
+          reference: '15.6.2', // FYI: There is possibility to change version
+        };
+        assert.deepEqual(actual, expected);
+        done();
+      });
+    });
+
+    it('Semver ~', (done: any) => {
+      getPinnedReference({
+        name: 'react',
+        reference: '~15.3.0',
+      }).then((pkg: IPackage) => {
+        const actual: IPackage = pkg;
+        const expected: IPackage = {
+          name: 'react',
+          reference: '15.3.2', // FYI: There is possibility to change version
+        };
+        assert.deepEqual(actual, expected);
+        done();
+      });
+    });
+
+    it('Semver fixed version', (done: any) => {
+      getPinnedReference({
+        name: 'react',
+        reference: '15.3.0',
+      }).then((pkg: IPackage) => {
+        const actual: IPackage = pkg;
+        const expected: IPackage = {
+          name: 'react',
+          reference: '15.3.0',
+        };
+        assert.deepEqual(actual, expected);
+        done();
+      });
+    });
+
+    it('Semver fixed file', (done: any) => {
+      getPinnedReference({
+        name: 'react',
+        reference: '/tmp/react-15.3.2.tar.gz',
+      }).then((pkg: IPackage) => {
+        const actual: IPackage = pkg;
+        const expected: IPackage = {
+          name: 'react',
+        reference: '/tmp/react-15.3.2.tar.gz',
+        };
+        assert.deepEqual(actual, expected);
+        done();
+      });
+    });
+  });
+
+  describe('fetchPackage', () => {
+    it('Runable', (done: any) => {
+      fetchPackage({
+        name: 'react',
+        reference: '15.4.1',
+      }).then(() => {
+        done();
+      });
+    });
   });
 });
